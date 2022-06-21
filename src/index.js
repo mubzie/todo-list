d7f1140c9efbec6ee82bb109d786d3fd945928f0
mubzie <rabiumubarak92@gmail.com>
2022-06-21 18:49:34 +0100

    still working on the UI implementation. no logic yet

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import { createUi } from './modules/createUi';
+
+// const render = () => {
+//     const content = document.getElementById('content');
+
+//     // content.appendChild(createUi());
+
+//     return content;
+
+// }
+
+document.body.appendChild(createUi());
