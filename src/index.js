5aab84e508ebd9c1674a842737bccf5a8a1a294f
mubzie <rabiumubarak92@gmail.com>
2022-06-30 12:45:29 +0100

    refactored taskform.js to html

--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
 import { addTaskForm } from "./modules/taskForm";
 import { displayTask } from "./modules/displayTask";
 
-const addTaskBtn = document.getElementById('add-task-icon');
-
-addTaskBtn.addEventListener('click', addTaskForm)
 
 // document.body.appendChild(addTaskForm());
--- src/modules/taskForm.js
+++ src/modules/taskForm.js
@@ -1,109 +1,27 @@
-   import { task } from "./task";
-   import { AddTaskFactory } from "./task";
-   import { displayTask } from "./displayTask";
-   import '../styles/formStyle.css'
+import { task } from "./task";
+import { AddTaskFactory } from "./task";
+import { displayTask } from "./displayTask";
+import '../styles/formStyle.css'
 
-   //helper function
-   const setAttribute = (elem, attri) =>{
-    Object.entries(attri).forEach(([key, value]) => elem.setAttribute(key, value));
-    }
-  
-   const addTaskForm = () => {
-  
-      const wrapperContainer = document.getElementById('big-modal');
-      wrapperContainer.classList.add('active');
 
-      const formContainer = document.createElement('div');
-      formContainer.classList.add('form-container');
+const addTaskBtn = document.getElementById('add-task-icon');
+let addTask = document.getElementById('addTaskForm')
+const closeFormBtn = document.getElementsByClassName('close-form-btn')[0];
+let formModal = document.querySelector('.big-modal');
 
-      const overlay = document.createElement('div');
-      overlay.classList.add('overlay');
-  
-      const form = document.createElement('form')
-      setAttribute(form, {'id': 'addTaskForm'})
-      form.classList.add('task-form');
 
-      const taskHeader = document.createElement('div');
-      taskHeader.classList.add('task-header');
-      taskHeader.textContent = 'Add New Task';
-  
-      //task input
-      const titleDiv = document.createElement('div');
-      titleDiv.classList.add('form-row');
-  
-      const titleLabel = document.createElement('label');
-      setAttribute(titleLabel, {'for': 'title'});
-      titleLabel.textContent = 'title';
-  
-      const titleInput = document.createElement('input');
-      setAttribute(titleInput, {'type': 'text', 'name': '', 'id': 'title', 'required': ''});
-      titleDiv.append(titleLabel, titleInput);
-  
-      //task description input
-      const DescriptionDiv = document.createElement('div');
-      DescriptionDiv.classList.add('form-row')
-  
-      const DescriptionLabel = document.createElement('label');
-      setAttribute(DescriptionLabel, {'for': 'description'});
-      DescriptionLabel.textContent = 'description';
-  
-      const DescriptionInput = document.createElement('textarea');
-      setAttribute(DescriptionInput, {'type': 'text', 'name': '', 'id': 'description', 'rows': '4', 'cols': '50'})
-      DescriptionDiv.append(DescriptionLabel, DescriptionInput);
-  
-      //date input
-      const dateDiv = document.createElement('div');
-      dateDiv.classList.add('form-row');
-  
-      const dateLabel = document.createElement('label');
-      setAttribute(dateLabel, {'for': 'date'})
-      dateLabel.textContent = 'Date';
-  
-      const dateInput = document.createElement('input');
-      setAttribute(dateInput, {'type': 'date', 'name': '', 'id': 'date'});
-      dateDiv.append(dateLabel, dateInput);
-  
-      //priority input
-      const priorityDiv = document.createElement('div');
-      priorityDiv.classList.add('form-row');
-  
-      const priorityLabel = document.createElement('label');
-      setAttribute(priorityLabel, {'for': 'priority'})
-      priorityLabel.textContent = 'Priority';
-  
-      const prioritySelection = document.createElement('select');
-      setAttribute(prioritySelection, {'type': 'text', 'name': '', 'id': 'priority'});
-  
-      let priorityOption = ["high","medium","low"];
-  
-      for (let i = 0; i < priorityOption.length; i++) {
-          let option = document.createElement("option");
-          setAttribute(option, {'vale': priorityOption[i]});
-          option.textContent = priorityOption[i];
-          prioritySelection.appendChild(option);
-      } 
-  
-      priorityDiv.append(priorityLabel, prioritySelection);
-  
-      const buttonDiv = document.createElement('div');
-      buttonDiv.classList.add('form-button');
-  
-      const submitButton = document.createElement('button');
-      submitButton.classList.add('submit')
-      submitButton.textContent = 'add task'
-      setAttribute(submitButton, {'type': 'submit'});
+addTaskBtn.addEventListener('click', () => {
+    formModal.classList.add('active');
+    console.log('bro');
+})
 
-      buttonDiv.appendChild(submitButton);
-  
-      form.append(taskHeader, titleDiv, DescriptionDiv, dateDiv, priorityDiv, buttonDiv)
-  
-      formContainer.appendChild(form);
-      
-      wrapperContainer.append(overlay, formContainer);
+const closeForm = () => {
+    formModal.classList.remove("active")
+}
 
-      // document.body.appendChild(formContainer);
+closeFormBtn.addEventListener('click', closeForm)
 
-      const addTaskFunction = (e) => {
+const addTaskFunction = (e) => {
         e.preventDefault();
   
         const titleInput = document.getElementById('title').value;
@@ -116,16 +34,13 @@
         console.log(task);
   
         displayTask(task, task);
+        closeForm();
   
-      }
+}
   
-      let addTask = document.getElementById('addTaskForm');
-  
-      addTask.addEventListener('submit', addTaskFunction);
+addTask.addEventListener('submit', addTaskFunction);
+
+export { addTaskFunction }
 
 
-    }
-
-
-export { addTaskForm };
 
